7a2487341b028bdd9eb866f8858901c21c298a42
Omar Elshimi <70725794+Omarelshimi@users.noreply.github.com>
2020-09-23 13:23:12 -0600

    Add files via upload

---
+++ Nickname Generator Assignment/main.js
@@ -0,0 +1,19 @@
+// Global Variable
+let names = ("Tyrannosaurus", "Stegosaurus", "Triceratops", "Velociraptor", 
+"Spinosaurus", "Allosaurus", "Archaeopteryx")
+
+// Functions
+document.getElementById("random").addEventListener('click', randomN)
+document.getElementById("all").addEventListener('click', allN)
+
+function randomN() {
+
+    let firstname = document.getElementById('first').value;
+   
+}
+
+function allN() {
+
+    let firstname = document.getElementById('first').value;
+    
+}
