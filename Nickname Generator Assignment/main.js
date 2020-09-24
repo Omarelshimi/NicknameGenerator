16259f7b6366f10daffb6a63c39335433cf4bde7
Omar Elshimi <70725794+Omarelshimi@users.noreply.github.com>
2020-09-24 13:07:49 -0600

    Add files via upload

--- Nickname Generator Assignment/main.js
+++ Nickname Generator Assignment/main.js
@@ -1,6 +1,7 @@
 // Global Variable
-let names = ("Tyrannosaurus", "Stegosaurus", "Triceratops", "Velociraptor", 
-"Spinosaurus", "Allosaurus", "Archaeopteryx")
+let names = ["Tyrannosaurus", "Stegosaurus", "Triceratops", "Velociraptor",
+    "Spinosaurus", "Allosaurus", "Archaeopteryx"]
+let nickIndex = 0;
 
 // Functions
 document.getElementById("random").addEventListener('click', randomN)
@@ -9,11 +10,23 @@ document.getElementById("all").addEventListener('click', allN)
 function randomN() {
 
     let firstname = document.getElementById('first').value;
-   
+    let lastname = document.getElementById('last').value;
+
+    let nickIndex = Math.randomInt(0, 7)
+
+    document.getElementById('result').innerHTML = firstname + " " + '"' + names[nickIndex] + '"' + " " + lastname + '<br>';
+
 }
 
 function allN() {
+    document.getElementById('result').innerHTML = ''
 
     let firstname = document.getElementById('first').value;
-    
+    let lastname = document.getElementById('last').value;
+
+    for (let i = 0; i < names.length; i++) {
+        document.getElementById('result').innerHTML += firstname + " " + '"' + names[i] + '"' + " " + lastname + '<br>';
+
+    }
+
 }
